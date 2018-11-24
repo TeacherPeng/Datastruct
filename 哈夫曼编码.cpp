876dba42aedb404be435f22af977ac8dd3217a0c
Teacher Peng <pengsw@mail.buct.edu.cn>
2018-11-24 10:50:14 +0800

    Update 哈夫曼编码.cpp

--- 哈夫曼编码.cpp
+++ 哈夫曼编码.cpp
@@ -22,6 +22,70 @@ struct HuffmanTree
     int leafnumber;
 };
 
+// 在哈夫曼树[0~n-1]范围内查找根结点权值最小的k1和根结点权值次小的k2
+int select(HuffmanTree &T, int n, int &k1, int &k2)
+{
+    int i1 = -1, i2 = -2;
+    for (int i = 0; i < n; i++)
+    {
+        // 略过非根结点
+        if (T.nodes[i].parent != -1)
+            continue;
+        if (i1 == -1 || T.nodes[i].weight < T.nodes[i1].weight)
+        {
+            // 发现一个比目前已知权值最小的i1更小的根结点i
+            // 原来的最小变成了次小
+            i2 = i1;
+            i1 = i;
+        }
+        else if (i2 == -1 || T.nodes[i].weight < T.nodes[i2].weight)
+        {
+            // 发现一个比目前已知权值次小的i2更小的根结点i
+            i2 = i;
+        }
+    }
+
+    // 约定先出现的（低下标）在左子
+    k1 = min(i1, i2);
+    k2 = max(i1, i2);
+    return 0;
+}
+
+// 采用顺序查找权值最小次小根结点的策略构造哈夫曼树
+int CreateHuffmanTree2(HuffmanTree &T, int leafnumber, int *weights)
+{
+    // 分配哈夫曼树存储空间
+    T.nodes = new HuffmanNode[leafnumber + leafnumber - 1];
+    T.leafnumber = leafnumber;
+
+    // 初始化叶结点
+    for (int i = 0; i < leafnumber; i++)
+    {
+        T.nodes[i].index = i;
+        T.nodes[i].weight = weights[i];
+        T.nodes[i].parent = T.nodes[i].lchild = T.nodes[i].rchild = -1;
+    }
+
+    // 开始构造哈夫曼树
+    for (int i = 0; i < leafnumber - 1; i++)
+    {
+        // 选择权值最小和次小的根结点k1, k2，且k1 < k2
+        int k1, k2;
+        select(T, leafnumber + i, k1, k2);
+
+        // 将根结点k1, 根结点k2合并到新增根结点i + leafnumber下
+        T.nodes[i + leafnumber].weight = T.nodes[k1].weight + T.nodes[k2].weight;
+        T.nodes[i + leafnumber].parent = -1;
+        T.nodes[i + leafnumber].lchild = k1;
+        T.nodes[i + leafnumber].rchild = k2;
+        T.nodes[k1].parent = i + leafnumber;
+        T.nodes[k2].parent = i + leafnumber;
+    }
+
+    return 0;
+}
+
+// 采用对叶结点预排序策略构造哈夫曼树
 int CreateHuffmanTree(HuffmanTree &T, int leafnumber, int *weights)
 {
     // 分配哈夫曼树存储空间
@@ -164,7 +228,7 @@ int main()
     int weights[] = {13, 7, 8, 6, 21, 3, 9, 2}; // 假设对应字符为a~e
 
     cout << "创建哈夫曼树……" << endl;
-    CreateHuffmanTree(T, sizeof(weights)/sizeof(*weights), weights);
+    CreateHuffmanTree(T, sizeof(weights) / sizeof(*weights), weights);
 
     cout << "自顶向下创建哈夫曼编码……" << endl;
     vector<string> aCodes0 = CreateHuffmanCode2(T);
