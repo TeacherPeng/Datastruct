80b0a260d027385def8a1400cc983dbac52983d6
Teacher Peng <pengsw@mail.buct.edu.cn>
2018-11-25 14:45:59 +0800

    添加关于命令行参数的文件的字节读写操作的示例代码。

---
+++ demo01.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+// 显示使用说明
+int ShowHelp()
+{
+    cout << "Usage: demo01 [z|x] sourcefile destfile" << endl;
+    cout << "    z means zip sourcefile to destfile, x means extract sourcefile to destfile." << endl;
+    return 0;
+}
+
+// 带命令行参数的main函数
+int main(int argc, char *argv[])
+{
+    cout << "命令行参数：" << endl;
+    for (int i = 0; i < argc; i++)
+    {
+        cout << "arg[" << i << "]: " << argv[i] << endl;
+    }
+
+    // 检查命令行参数，如果没有读到所需要的命令行参数，提示使用说明
+    if (argc != 4)
+    {
+        ShowHelp();
+    }
+    else if (stricmp(argv[1], "z") == 0)
+    {
+        cout << "Zip " << argv[2] << " to " << argv[3] << " ..." << endl;
+    }
+    else if (stricmp(argv[1], "e") == 0)
+    {
+        cout << "Extract " << argv[2] << " to " << argv[3] << " ..." << endl;
+    }
+    else
+    {
+        ShowHelp();
+    }
+
+    return 0;
+}
---
+++ demo02.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        cerr << "Usage: demo02 sourcefilename destfilename" << endl;
+        return 0;
+    }
+
+    // 打开输入文件流
+    ifstream instr(argv[1], ios::in | ios::binary);
+    if (!instr)
+    {
+        cerr << "Open " << argv[1] << " failure." << endl;
+        return 0;
+    }
+
+    // 打开输出文件流
+    ofstream outstr(argv[2], ios::out | ios::binary);
+    if (!outstr)
+    {
+        cerr << "Create " << argv[2] << " failure." << endl;
+        instr.close();
+        return 0;
+    }
+
+    // 从输入文件流逐字节读入，并写到输出文件流中
+    while (!instr.eof())
+    {
+        int ch = instr.get();
+        if (ch != EOF) outstr.put(ch);
+    }
+
+    // 关闭文件流
+    instr.close();
+    outstr.close();
+
+    return 0;
+}
