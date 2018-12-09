ed6f6177381a7edfea8f140d5fba0e8cfac342af
Teacher Peng <pengsw@mail.buct.edu.cn>
2018-12-09 20:21:28 +0800

    更新位向量集合教案和代码，更新二叉排序树删除操作教案和代码。

--- 二叉排序树.cpp
+++ 二叉排序树.cpp
@@ -4,139 +4,224 @@ using namespace std;
 
 struct BiNode
 {
-	BiNode(int aKey)
-	{
-		key = aKey;
-		lchild = rchild = NULL;
-	}
-	int key;
-	BiNode *lchild, *rchild;
+    BiNode(int aKey)
+    {
+        key = aKey;
+        lchild = rchild = nullptr;
+    }
+    int key;
+    BiNode *lchild, *rchild;
 };
 typedef BiNode *BiTree;
 
 int InitBiTree(BiTree &T)
 {
-	T = NULL;
-	return 0;
+    T = nullptr;
+    return 0;
 }
 
 int DestroyBiTree(BiTree &T)
 {
-	if (T == NULL) return 0;
-	DestroyBiTree(T->lchild);
-	DestroyBiTree(T->rchild);
-	delete T;
-	return 0;
+    if (T == nullptr)
+        return 0;
+    DestroyBiTree(T->lchild);
+    DestroyBiTree(T->rchild);
+    delete T;
+    return 0;
 }
 
 BiNode *BSTSearch(BiTree &T, int aKey)
 {
-	BiNode *p = T;
-	while (p != NULL && p->key != aKey)
-	{
-		p = aKey < p->key ? p->lchild : p->rchild;
-	}
-	return p;
+    BiNode *p = T;
+    while (p != nullptr && p->key != aKey)
+    {
+        p = aKey < p->key ? p->lchild : p->rchild;
+    }
+    return p;
 }
 
 int Insert(BiTree &T, int aKey)
 {
-	if (T == NULL)
-	{
-		T = new BiNode(aKey);
-		return 0;
-	}
+    if (T == nullptr)
+    {
+        T = new BiNode(aKey);
+        return 0;
+    }
 
-	BiNode *p = T;
-	while (p != NULL && p->key != aKey)
-	{
-		if (aKey < p->key)
-		{
-			if (p->lchild == NULL)
-			{
-				p->lchild = new BiNode(aKey);
-				return 0;
-			}
-			else
-				p = p->lchild;
-		}
-		else
-		{
-			if (p->rchild == NULL)
-			{
-				p->rchild = new BiNode(aKey);
-				return 0;
-			}
-			else
-				p = p->rchild;
-		}
-	}
-	return 1;
+    BiNode *p = T;
+    while (p != nullptr && p->key != aKey)
+    {
+        if (aKey < p->key)
+        {
+            if (p->lchild == nullptr)
+            {
+                p->lchild = new BiNode(aKey);
+                return 0;
+            }
+            else
+                p = p->lchild;
+        }
+        else
+        {
+            if (p->rchild == nullptr)
+            {
+                p->rchild = new BiNode(aKey);
+                return 0;
+            }
+            else
+                p = p->rchild;
+        }
+    }
+    return 1;
 }
 
 int Insert2(BiTree &T, int aKey)
 {
-	BiNode **p = &T;
-	while (*p != NULL && (*p)->key != aKey)
-	{
-		p = aKey < (*p)->key ? &(*p)->lchild : &(*p)->rchild;
-	}
-	if (*p != NULL) return 1;
-	*p = new BiNode(aKey);
-	return 0;
+    // 定位插入位置
+    BiNode **p = &T;
+    while (*p != nullptr && (*p)->key != aKey)
+    {
+        p = aKey < (*p)->key ? &(*p)->lchild : &(*p)->rchild;
+    }
+    if (*p != nullptr)
+        return 1;
+
+    // 插入新结点
+    *p = new BiNode(aKey);
+    return 0;
+}
+
+int Remove(BiTree &T, int aKey)
+{
+    // 定位被删除结点
+    BiNode **p = &T;
+    while (*p != nullptr && (*p)->key != aKey)
+    {
+        p = aKey < (*p)->key ? &(*p)->lchild : &(*p)->rchild;
+    }
+    if (*p == nullptr)
+        return 1;
+
+    // 保存待删除的结点
+    BiNode *willRemove = *p;
+
+    if ((*p)->lchild == nullptr)
+    {
+        // 如果没有左子，直接用右子结点替换被删除结点
+        *p = (*p)->rchild;
+    }
+    else if ((*p)->rchild == nullptr)
+    {
+        // 如果没有右子，直接用左子结点替换被删除结点
+        *p = (*p)->lchild;
+    }
+    else
+    {
+        // 找到右子的最左结点
+        BiNode **q = &((*p)->rchild);
+        while ((*q)->lchild != nullptr)
+            *q = (*q)->lchild;
+
+        // 用右子最左结点的右子替换最左结点
+        BiNode *temp = *q;
+        *q = (*q)->rchild;
+
+        // 用最左结点替换被删除结点
+        temp->lchild = (*p)->lchild;
+        temp->rchild = (*p)->rchild;
+        *p = temp;
+    }
+
+    // 释放待删除结点
+    delete willRemove;
+    return 0;
 }
 
 int PreTraverse(BiTree T)
 {
-	if (T == NULL) return 0;
-	cout << T->key << ", ";
-	PreTraverse(T->lchild);
-	PreTraverse(T->rchild);
-	return 0;
+    if (T == nullptr)
+        return 0;
+    cout << T->key << ", ";
+    PreTraverse(T->lchild);
+    PreTraverse(T->rchild);
+    return 0;
 }
 int InTraverse(BiTree T)
 {
-	if (T == NULL) return 0;
-	InTraverse(T->lchild);
-	cout << T->key << ", ";
-	InTraverse(T->rchild);
-	return 0;
+    if (T == nullptr)
+        return 0;
+    InTraverse(T->lchild);
+    cout << T->key << ", ";
+    InTraverse(T->rchild);
+    return 0;
 }
 int SucTraverse(BiTree T)
 {
-	if (T == NULL) return 0;
-	SucTraverse(T->lchild);
-	SucTraverse(T->rchild);
-	cout << T->key << ", ";
-	return 0;
+    if (T == nullptr)
+        return 0;
+    SucTraverse(T->lchild);
+    SucTraverse(T->rchild);
+    cout << T->key << ", ";
+    return 0;
 }
 
 int CreateRands(int datas[], int n, int m)
 {
-	for (int i = 0; i < n; i++)
-		datas[i] = i + 1;
-	srand((unsigned int)time(NULL));
-	for (int i = 0; i < m; i++)
-		swap(datas[rand() % n], datas[rand() % n]);
-	return 0;
+    for (int i = 0; i < n; i++)
+        datas[i] = i + 1;
+    srand((unsigned int)time(nullptr));
+    for (int i = 0; i < m; i++)
+        swap(datas[rand() % n], datas[rand() % n]);
+    return 0;
+}
+
+int Print(BiTree T)
+{
+    cout << "排序二叉树的前序遍历序：";
+    PreTraverse(T);
+    cout << endl;
+
+    cout << "排序二叉树的中序遍历序：";
+    InTraverse(T);
+    cout << endl;
+
+    cout << "排序二叉树的后序遍历序：";
+    SucTraverse(T);
+    cout << endl;
+
+    return 0;
 }
 
 int main()
 {
-	int datas[16], n = 16, m = 20;
-	CreateRands(datas, n, m);
-	BiTree T;
-	InitBiTree(T);
-	for (int i = 0; i < n; i++)
-	{
-		cout << datas[i] << ", ";
-		Insert(T, datas[i]);
-	}
-	cout << endl;
-	PreTraverse(T); cout << endl;
-	InTraverse(T); cout << endl;
-	SucTraverse(T); cout << endl;
-	DestroyBiTree(T);
-	system("pause");
-	return 0;
+    constexpr int n = 16, m = 20;
+    int datas[n];
+    CreateRands(datas, n, m);
+    cout << "输入序列：";
+    for (int i = 0; i < n; i++)
+    {
+        cout << datas[i] << ", ";
+    }
+    cout << endl;
+
+    cout << "构造排序二叉树……" << endl;
+    BiTree T;
+    InitBiTree(T);
+    for (int i = 0; i < n; i++)
+    {
+        Insert(T, datas[i]);
+    }
+    Print(T);
+
+    cout << "删除8：" << endl;
+    Remove(T, 8);
+    Print(T);
+
+    cout << "插入8：" << endl;
+    Insert2(T, 8);
+    Print(T);
+
+    DestroyBiTree(T);
+    system("pause");
+    return 0;
 }
---
+++ 集合上的位运算.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+using SmallSet = long; // typedef long SmallSet;
+
+int Set(SmallSet &aSet, int aIndex)
+{
+    aSet |= (1 << aIndex);
+    return 0;
+}
+
+int Reset(SmallSet &aSet, int aIndex)
+{
+    aSet &= ~(1 << aIndex);
+    return 0;
+}
+
+int Reverse(SmallSet &aSet, int aIndex)
+{
+    aSet ^= (1 << aIndex);
+    return 0;
+}
+
+SmallSet ComplementarySet(SmallSet aFullSet, SmallSet aSet)
+{
+    return aFullSet & ~aSet;
+}
+
+SmallSet IntersectionSet(SmallSet aSet1, SmallSet aSet2)
+{
+    return aSet1 & aSet2;
+}
+
+SmallSet UnionSet(SmallSet aSet1, SmallSet aSet2)
+{
+    return aSet1 | aSet2;
+}
+
+SmallSet DifferenceSet(SmallSet aSet1, SmallSet aSet2)
+{
+    return aSet1 ^ aSet2;
+}
+
+bool Has(SmallSet aSet, int aIndex)
+{
+    return (aSet & (1 << aIndex)) != 0;
+}
+
+int Print(SmallSet aSet)
+{
+    cout << "{";
+    for (int aIndex = 0; aSet != 0; aIndex++, aSet >>= 1)
+    {
+        if (Has(aSet, 0))
+            cout << aIndex << ", ";
+    }
+    cout << "}" << endl;
+    return 0;
+}
+
+int main()
+{
+    SmallSet aSet = 11;
+    Print(aSet);
+
+    cout << "Set 4: ";
+    Set(aSet, 4);
+    Print(aSet);
+
+    cout << "Reset 3: ";
+    Reset(aSet, 3);
+    Print(aSet);
+
+    cout << "Complementary :";
+    SmallSet aSet2 = ComplementarySet(0xff, aSet);
+    Print(aSet2);
+
+    cout << "Reverse 5: ";
+    Reverse(aSet, 5);
+    Print(aSet);
+
+    cout << "Reverse 5: ";
+    Reverse(aSet, 5);
+    Print(aSet);
+
+    return 0;
+}
