06cbb8bf2860e2e5c22353d53f6e0df16545bb12
Teacher Peng <pengsw@mail.buct.edu.cn>
2018-12-18 09:19:14 +0800

    更新了平衡二叉树的示例代码和教案。

--- 平衡二叉树.cpp
+++ 平衡二叉树.cpp
@@ -26,83 +26,83 @@ AVLNode::~AVLNode()
     rchild = nullptr;
 }
 
-int LRotate(AVLNode **p)
+int LRotate(AVLNode *&p)
 {
-    AVLNode *temp = *p;
-    *p = (*p)->rchild;
-    temp->rchild = (*p)->lchild;
-    (*p)->lchild = temp;
+    AVLNode *temp = p;
+    p = p->rchild;
+    temp->rchild = p->lchild;
+    p->lchild = temp;
     return 0;
 }
 
-int RRotate(AVLNode **p)
+int RRotate(AVLNode *&p)
 {
-    AVLNode *temp = *p;
-    *p = (*p)->lchild;
-    temp->lchild = (*p)->rchild;
-    (*p)->rchild = temp;
+    AVLNode *temp = p;
+    p = p->lchild;
+    temp->lchild = p->rchild;
+    p->rchild = temp;
     return 0;
 }
 
-int LLAdjust(AVLNode **p)
+int LLAdjust(AVLNode *&p)
 {
     RRotate(p);
-    (*p)->balance = 0;
-    (*p)->rchild->balance = 0;
+    p->balance = 0;
+    p->rchild->balance = 0;
     return 0;
 }
 
-int LRAdjust(AVLNode **p)
+int LRAdjust(AVLNode *&p)
 {
-    LRotate(&(*p)->lchild);
+    LRotate(p->lchild);
     RRotate(p);
-    switch ((*p)->balance)
+    switch (p->balance)
     {
     case 1:
-        (*p)->lchild->balance = 0;
-        (*p)->rchild->balance = -1;
+        p->lchild->balance = 0;
+        p->rchild->balance = -1;
         break;
     case 0:
-        (*p)->lchild->balance = 0;
-        (*p)->rchild->balance = 0;
+        p->lchild->balance = 0;
+        p->rchild->balance = 0;
         break;
     case -1:
-        (*p)->lchild->balance = 1;
-        (*p)->rchild->balance = 0;
+        p->lchild->balance = 1;
+        p->rchild->balance = 0;
         break;
     }
-    (*p)->balance = 0;
+    p->balance = 0;
     return 0;
 }
 
-int RLAdjust(AVLNode **p)
+int RLAdjust(AVLNode *&p)
 {
-    RRotate(&(*p)->rchild);
+    RRotate(p->rchild);
     LRotate(p);
-    switch ((*p)->balance)
+    switch (p->balance)
     {
     case -1:
-        (*p)->lchild->balance = 1;
-        (*p)->rchild->balance = 0;
+        p->lchild->balance = 1;
+        p->rchild->balance = 0;
         break;
     case 0:
-        (*p)->lchild->balance = 0;
-        (*p)->rchild->balance = 0;
+        p->lchild->balance = 0;
+        p->rchild->balance = 0;
         break;
     case 1:
-        (*p)->lchild->balance = 0;
-        (*p)->rchild->balance = -1;
+        p->lchild->balance = 0;
+        p->rchild->balance = -1;
         break;
     }
-    (*p)->balance = 0;
+    p->balance = 0;
     return 0;
 }
 
-int RRAdjust(AVLNode **p)
+int RRAdjust(AVLNode *&p)
 {
     LRotate(p);
-    (*p)->balance = 0;
-    (*p)->lchild->balance = 0;
+    p->balance = 0;
+    p->lchild->balance = 0;
     return 0;
 }
 
@@ -132,42 +132,45 @@ int Insert(AVLTree &T, int aKey)
     // 自底向上沿祖先链修正各祖先结点的平衡因子
     while (!ancestors.empty())
     {
-        auto aParent = ancestors.top();
+        // 修正祖先结点的平衡因子
+        auto aPair = ancestors.top();
         ancestors.pop();
-        p = aParent.first;
-        (*p)->balance += aParent.second;
-        switch ((*p)->balance)
+        AVLNode *&curr = *aPair.first;
+        curr->balance += aPair.second;
+
+        // 检查平衡状态
+        switch (curr->balance)
         {
         case 0:
-            // 上面的结点的平衡因子不会再变化，结束修正过程
+            // 祖先结点的平衡因子不会再变化，结束修正过程
             return 0;
         case -1:
         case 1:
             // 没有失衡，但平衡因子的变化还需要继续向上传递
             break;
         case 2: // 新结点在左子树上
-            switch ((*p)->lchild->balance)
+            switch (curr->lchild->balance)
             {
             case 1:
                 // 新结点在左子的左子上（LL型）
-                LLAdjust(p);
+                LLAdjust(curr);
                 break;
             case -1:
                 // 新结点在左子的右子上（LR型）
-                LRAdjust(p);
+                LRAdjust(curr);
                 break;
             }
             return 0;
         case -2: // 新结点在右子树上
-            switch ((*p)->rchild->balance)
+            switch (curr->rchild->balance)
             {
             case 1:
                 // 新结点在右子的左子上（RL型）
-                RLAdjust(p);
+                RLAdjust(curr);
                 break;
             case -1:
                 // 新结点在右子的右子上（RR型）
-                RRAdjust(p);
+                RRAdjust(curr);
                 break;
             }
             return 0;
