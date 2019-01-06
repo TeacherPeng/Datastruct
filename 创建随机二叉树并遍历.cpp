7f7e119e028a9bb36e358c1a17c4f8a6cd7b6884
Teacher Peng <pengsw@mail.buct.edu.cn>
2019-01-06 15:59:50 +0800

    更新二叉树代码，添加复习教案和参考资料。

---
+++ 创建随机二叉树并遍历.cpp
@@ -0,0 +1,184 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <stack>
+using namespace std;
+int Shuffle(char *nodes, int n)
+{
+    for (int i = 0; i < 10 * n; i++)
+    {
+        int x = rand() % n;
+        int y = rand() % n;
+        swap(nodes[x], nodes[y]);
+    }
+    return 0;
+}
+struct BiNode
+{
+    char data;
+    BiNode *lchild, *rchild;
+};
+typedef BiNode *BiTree;
+int DestroyBiTree(BiTree &T)
+{
+    if (T == NULL)
+        return 0;
+    DestroyBiTree(T->lchild);
+    DestroyBiTree(T->rchild);
+    delete T;
+    return 0;
+}
+BiTree CreateBiTree(const char *nodes)
+{
+    BiTree T = NULL;
+    for (const char *p = nodes; *p != '\0'; p++)
+    {
+        BiTree *tp = &T;
+        while (*tp != NULL)
+        {
+            if ((rand() % 2) == 0)
+                tp = &((*tp)->lchild);
+            else
+                tp = &((*tp)->rchild);
+        }
+        *tp = new BiNode;
+        (*tp)->data = *p;
+        (*tp)->lchild = (*tp)->rchild = NULL;
+    }
+    return T;
+}
+int VPreTraverse(BiTree T)
+{
+    if (T == NULL)
+    {
+        cout << "#";
+        return 1;
+    }
+    cout << T->data;
+    VPreTraverse(T->lchild);
+    VPreTraverse(T->rchild);
+    return 0;
+}
+int PreTraverse(BiTree T)
+{
+    if (T == NULL)
+        return 0;
+    cout << T->data;
+    PreTraverse(T->lchild);
+    PreTraverse(T->rchild);
+    return 0;
+}
+int PreTraverse_Norecursion(BiTree T)
+{
+    if (T == nullptr)
+        return 0;
+    stack<BiNode *> s;
+    s.push(T);
+    while (!s.empty())
+    {
+        BiNode *p = s.top();
+        s.pop();
+        cout << p->data;
+        if (p->rchild != nullptr)
+            s.push(p->rchild);
+        if (p->lchild != nullptr)
+            s.push(p->lchild);
+    }
+    return 0;
+}
+int InTraverse(BiTree T)
+{
+    if (T == NULL)
+        return 0;
+    InTraverse(T->lchild);
+    cout << T->data;
+    InTraverse(T->rchild);
+    return 0;
+}
+int InTraverse_Norecursion(BiTree T)
+{
+    stack<BiNode *> s;
+    BiNode *p = T;
+    while (p != nullptr || !s.empty())
+    {
+        if (p != nullptr)
+        {
+            s.push(p);
+            p = p->lchild;
+        }
+        else
+        {
+            p = s.top();
+            s.pop();
+            cout << p->data;
+            p = p->rchild;
+        }
+    }
+    return 0;
+}
+int SucTraverse(BiTree T)
+{
+    if (T == NULL)
+        return 0;
+    SucTraverse(T->lchild);
+    SucTraverse(T->rchild);
+    cout << T->data;
+    return 0;
+}
+int SucTraverse_Norecursion(BiTree T)
+{
+    if (T == nullptr)
+        return 0;
+    stack<pair<BiNode *, bool>> s;
+    s.push(make_pair(T, false));
+    while (!s.empty())
+    {
+        auto p = s.top();
+        s.pop();
+        if (p.second)
+        {
+            cout << p.first->data;
+        }
+        else
+        {
+            s.push(make_pair(p.first, true));
+            if (p.first->rchild != nullptr)
+                s.push(make_pair(p.first->rchild, false));
+            if (p.first->lchild != nullptr)
+                s.push(make_pair(p.first->lchild, false));
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *args[])
+{
+    srand((unsigned int)time(NULL));
+    char defaultstr[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+    int n = (rand() % strlen(defaultstr)) + 1;
+    char *s = defaultstr;
+    s[n] = '\0';
+    if (argc == 2)
+    {
+        s = args[1];
+        n = strlen(s);
+    }
+    Shuffle(s, n);
+    BiTree T = CreateBiTree(s);
+    VPreTraverse(T);
+    cout << endl;
+    PreTraverse(T);
+    cout << endl;
+    PreTraverse_Norecursion(T);
+    cout << endl;
+    InTraverse(T);
+    cout << endl;
+    InTraverse_Norecursion(T);
+    cout << endl;
+    SucTraverse(T);
+    cout << endl;
+    SucTraverse_Norecursion(T);
+    cout << endl;
+    DestroyBiTree(T);
+    return 0;
+}
---
+++ 用带虚结点的前序遍历序列构造二叉树.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int Shuffle(char *nodes, int n)
+{
+    for (int i = 0; i < 10 * n; i++)
+    {
+        int x = rand() % n;
+        int y = rand() % n;
+        swap(nodes[x], nodes[y]);
+    }
+    return 1;
+}
+struct BiNode
+{
+    char data;
+    BiNode *lchild, *rchild;
+};
+typedef BiNode *BiTree;
+int DestroyBiTree(BiTree &T)
+{
+    if (T == NULL)
+        return 0;
+    DestroyBiTree(T->lchild);
+    DestroyBiTree(T->rchild);
+    delete T;
+    return 1;
+}
+const char *CreateBiTree(BiTree &T, const char *s)
+{
+    if (s == nullptr || *s == '\0')
+        return nullptr;
+    if (*s=='#')
+    {
+        T = nullptr;
+        return s + 1;
+    }
+    T = new BiNode;
+    T->data = *s;
+    const char *afterLeft = CreateBiTree(T->lchild, s + 1);
+    if (afterLeft == nullptr)
+        return nullptr;
+    const char *afterRight = CreateBiTree(T->rchild, afterLeft);
+    return afterRight;
+}
+int PreBiTree(BiTree T)
+{
+    if (T == NULL)
+        return 1;
+    cout << T->data;
+    PreBiTree(T->lchild);
+    PreBiTree(T->rchild);
+    return 1;
+}
+int InBiTree(BiTree T)
+{
+    if (T == NULL)
+        return 1;
+    InBiTree(T->lchild);
+    cout << T->data;
+    InBiTree(T->rchild);
+    return 1;
+}
+int SucBiTree(BiTree T)
+{
+    if (T == NULL)
+        return 1;
+    SucBiTree(T->lchild);
+    SucBiTree(T->rchild);
+    cout << T->data;
+    return 1;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    BiTree T;
+    if (CreateBiTree(T, s.c_str()) == nullptr)
+    {
+        cout << "构造失败，输入串可能有问题！" << endl;
+        return 1;
+    }
+    PreBiTree(T);
+    cout << endl;
+    InBiTree(T);
+    cout << endl;
+    SucBiTree(T);
+    cout << endl;
+    DestroyBiTree(T);
+    return 1;
+}
